1c5bb6c33974408c23990120b3ab3c2f2b9bbc1b
Kevin-HB18 <kevinholguin18@gmail.com>
2023-11-20 22:28:09 -0500

    frontend registro

--- src/Registro/Registro.jsx
+++ src/Registro/Registro.jsx
@@ -5,6 +5,8 @@ import axios from "axios";
 
 
 export function Registrar() {
+  const [confirmationMessage, setConfirmationMessage] = useState(null);
+  const [totaldirecc, setTotalDirecc] = useState(0);  
 
   const [tipoDocumento, setTipoDocumento] = useState([]);
   const [tipoPersona, setTipoPersona] = useState([]);
@@ -25,10 +27,10 @@ export function Registrar() {
   });
 
   const [contacto, setContacto] = useState([
-    {IDTIPOCONTACTO:'', DESCTIPOCONTACTO:'', IDTIPOPERSONA:'', IDTIPODOC:'', NDOCUMENTO:'', DESCCONTACTO:''},
-    {IDTIPOCONTACTO:'', DESCTIPOCONTACTO:'', IDTIPOPERSONA:'', IDTIPODOC:'', NDOCUMENTO:'', DESCCONTACTO:''},
-    {IDTIPOCONTACTO:'', DESCTIPOCONTACTO:'', IDTIPOPERSONA:'', IDTIPODOC:'', NDOCUMENTO:'', DESCCONTACTO:''},
-    {IDTIPOCONTACTO:'', DESCTIPOCONTACTO:'', IDTIPOPERSONA:'', IDTIPODOC:'', NDOCUMENTO:'', DESCCONTACTO:''}
+    {IDTIPOCONTACTO:'', DESCTIPOCONTACTO:'', IDTIPOPERSONA:persona.IDTIPOPERSONA, IDTIPODOC:persona.IDTIPODOC, NDOCUMENTO:persona.NDOCUMENTO, DESCCONTACTO:''},
+    {IDTIPOCONTACTO:'', DESCTIPOCONTACTO:'', IDTIPOPERSONA:persona.IDTIPOPERSONA, IDTIPODOC:persona.IDTIPODOC, NDOCUMENTO:persona.NDOCUMENTO, DESCCONTACTO:''},
+    {IDTIPOCONTACTO:'', DESCTIPOCONTACTO:'', IDTIPOPERSONA:persona.IDTIPOPERSONA, IDTIPODOC:persona.IDTIPODOC, NDOCUMENTO:persona.NDOCUMENTO, DESCCONTACTO:''},
+    {IDTIPOCONTACTO:'', DESCTIPOCONTACTO:'', IDTIPOPERSONA:persona.IDTIPOPERSONA, IDTIPODOC:persona.IDTIPODOC, NDOCUMENTO:persona.NDOCUMENTO, DESCCONTACTO:''}
   ]);
 
   const [tipoVia, setTipoVia] = useState([]);
@@ -39,27 +41,27 @@ export function Registrar() {
   const [tipoPredio, setTipoPredio] = useState([]);
   const [tipocomplemento, setTipoComplemento] = useState([]); 
 
-  const [dir1, setDir1] = useState({ POSICION: 1, IDNOMEN: '', VALORDIRECC: '' });
-  const [dir2, setDir2] = useState({ POSICION: 2, IDNOMEN: '', VALORDIRECC: '' });
-  const [dir3, setDir3] = useState({ POSICION: 3, IDNOMEN: '', VALORDIRECC: '' });
-  const [dir4, setDir4] = useState({ POSICION: 4, IDNOMEN: '', VALORDIRECC: '' });
-  const [dir5, setDir5] = useState({ POSICION: 5, IDNOMEN: '', VALORDIRECC: '' });
-  const [dir6, setDir6] = useState({ POSICION: 6, IDNOMEN: '', VALORDIRECC: '' });
-  const [dir7, setDir7] = useState({ POSICION: 7, IDNOMEN: '', VALORDIRECC: '' });
-  const [dir8, setDir8] = useState({ POSICION: 8, IDNOMEN: '', VALORDIRECC: '' });
-  const [dir9, setDir9] = useState({ POSICION: 9, IDNOMEN: '', VALORDIRECC: '' });
-  const [dir10, setDir10] = useState({ POSICION: 10, IDNOMEN: '', VALORDIRECC: '' });
-  const [dir11, setDir11] = useState({ POSICION: 11, IDNOMEN: '', VALORDIRECC: '' });
-  const [dir12, setDir12] = useState({ POSICION: 12, IDNOMEN: '', VALORDIRECC: '' });
-  const [dir13, setDir13] = useState({ POSICION: 13, IDNOMEN: '', VALORDIRECC: '' });
-  const [dir14, setDir14] = useState({ POSICION: 14, IDNOMEN: '', VALORDIRECC: '' });
-  const [dir15, setDir15] = useState({ POSICION: 15, IDNOMEN: '', VALORDIRECC: '' });
-  const [dir16, setDir16] = useState({ POSICION: 16, IDNOMEN: '', VALORDIRECC: '' });
-  const [dir17, setDir17] = useState({ POSICION: 17, IDNOMEN: '', VALORDIRECC: '' });
-  const [dir18, setDir18] = useState({ POSICION: 18, IDNOMEN: '', VALORDIRECC: '' });
-  const [dir19, setDir19] = useState({ POSICION: 19, IDNOMEN: '', VALORDIRECC: '' });
-  const [dir20, setDir20] = useState({ POSICION: 20, IDNOMEN: '', VALORDIRECC: '' });
-  const [dir21, setDir21] = useState({ POSICION: 21, IDNOMEN: '', VALORDIRECC: '' });
+  const [dir1, setDir1] = useState({ POSICION: 1, IDNOMEN: '', VALORDIRECC: null });
+  const [dir2, setDir2] = useState({ POSICION: 2, IDNOMEN: null, VALORDIRECC: '' });
+  const [dir3, setDir3] = useState({ POSICION: 3, IDNOMEN: null, VALORDIRECC: '' });
+  const [dir4, setDir4] = useState({ POSICION: 4, IDNOMEN: null, VALORDIRECC: '' });
+  const [dir5, setDir5] = useState({ POSICION: 5, IDNOMEN: null, VALORDIRECC: '' });
+  const [dir6, setDir6] = useState({ POSICION: 6, IDNOMEN: '', VALORDIRECC: null });
+  const [dir7, setDir7] = useState({ POSICION: 7, IDNOMEN: null, VALORDIRECC: '' });
+  const [dir8, setDir8] = useState({ POSICION: 8, IDNOMEN: null, VALORDIRECC: '' });
+  const [dir9, setDir9] = useState({ POSICION: 9, IDNOMEN: null, VALORDIRECC: '' });
+  const [dir10, setDir10] = useState({ POSICION: 10, IDNOMEN: null, VALORDIRECC: '' });
+  const [dir11, setDir11] = useState({ POSICION: 11, IDNOMEN: null, VALORDIRECC: '' });
+  const [dir12, setDir12] = useState({ POSICION: 12, IDNOMEN: null, VALORDIRECC: '' });
+  const [dir13, setDir13] = useState({ POSICION: 13, IDNOMEN: '', VALORDIRECC: null });
+  const [dir14, setDir14] = useState({ POSICION: 14, IDNOMEN: null, VALORDIRECC: '' });
+  const [dir15, setDir15] = useState({ POSICION: 15, IDNOMEN: '', VALORDIRECC: null });
+  const [dir16, setDir16] = useState({ POSICION: 16, IDNOMEN: null, VALORDIRECC: '' });
+  const [dir17, setDir17] = useState({ POSICION: 17, IDNOMEN: '', VALORDIRECC: null });
+  const [dir18, setDir18] = useState({ POSICION: 18, IDNOMEN: null, VALORDIRECC: '' });
+  const [dir19, setDir19] = useState({ POSICION: 19, IDNOMEN: '', VALORDIRECC: null });
+  const [dir20, setDir20] = useState({ POSICION: 20, IDNOMEN: null, VALORDIRECC: '' });
+  const [dir21, setDir21] = useState({ POSICION: 21, IDNOMEN: '', VALORDIRECC: null });
  
 
   useEffect(() => {
@@ -163,14 +165,188 @@ export function Registrar() {
     fetchDataTipoUrbanizacion();
     fetchDataTipoManzana();
     fetchDataTipoPredio();
-    fetchDataTipoComplemento();
+    fetchDataTipoComplemento(); 
+    
 
     }, []);
 
+    const fetchDataPushPersona = async () => {
+      try {
+        const response = await axios.post('http://localhost:3001/api/verificarRegistro', 
+        { IDTIPOPERSONA: persona.IDTIPOPERSONA, IDTIPODOC: persona.IDTIPODOC ,NDOCUMENTO: persona.NDOCUMENTO });       
+        if (response.data.exists) {
+            setConfirmationMessage('Ya existe un registro en la BD');
+        } else {
+            // Si no existe, realizar el registro
+            setConfirmationMessage('Registro exitoso');
+            await axios.post('http://localhost:3001/api/insertarPersona', persona);            
+        }  
+      } catch (error) {
+      console.error("Error al mandar personas", error);
+      }          
+    };
+
+    const fetchDataPushContacto = async (i) => {
+      try {
+        if(contacto[i].IDTIPOCONTACTO!=='' && contacto[i].DESCCONTACTO!=='')
+          await axios.post('http://localhost:3001/api/insertarcontacto', {IDTIPOCONTACTO: contacto[i].IDTIPOCONTACTO, DESCTIPOCONTACTO: contacto[i].DESCTIPOCONTACTO, IDTIPOPERSONA:persona.IDTIPOPERSONA, IDTIPODOC:persona.IDTIPODOC, NDOCUMENTO: persona.NDOCUMENTO, DESCCONTACTO: contacto[i].DESCCONTACTO });
+      } catch (error) {
+      console.error("Error al mandar contactos", error);
+      }          
+    };
+
+    const fetchDataPushDireccion = async (posicion,idnomen,valordirecc) =>{
+      try {
+        if((idnomen!=='' && valordirecc===null) || (idnomen===null && valordirecc!==''))
+          await axios.post('http://localhost:3001/api/insertardireccion', {POSICION: posicion, IDDIRECCION: totaldirecc, IDTIPOPERSONA:persona.IDTIPOPERSONA, IDTIPODOC:persona.IDTIPODOC, NDOCUMENTO: persona.NDOCUMENTO, IDNOMEN: idnomen, VALORDIREC: valordirecc});
+      } catch (error) {
+      console.error("Error al mandar direcciones", error);
+      }   
+    };
+
+    const fetchDataObtainDirecc = async () => {
+      try {
+        const response = await axios.get('http://localhost:3001/api/obtenerregistrosdireccion');       
+        setTotalDirecc(response.data);
+        setTotalDirecc(totaldirecc+1);
+      } catch (error) {
+      console.error("Error al mandar contactos", error);
+      }          
+    };
+  
     const handleSubmit  = async (e) => {
       e.preventDefault();
-      
+      setConfirmationMessage('');
+      try {               
+        fetchDataPushPersona();
 
+        setTimeout(() => {          
+          fetchDataPushContacto(0);          
+        }, 4000);
+        setTimeout(() => {         
+          fetchDataPushContacto(1);          
+        }, 4000); 
+        setTimeout(() => {          
+          fetchDataPushContacto(2);          
+        }, 4000);
+        setTimeout(() => {          
+          fetchDataPushContacto(3);         
+        }, 4000);
+
+        setTimeout(() => {          
+          fetchDataObtainDirecc();        
+        }, 1000);        
+        
+
+        //---------insertar direccion-------------        
+        setTimeout(() => {          
+          fetchDataPushDireccion(dir1.POSICION,dir1.IDNOMEN,dir1.VALORDIRECC);      
+        }, 2000);        
+
+        setTimeout(() => {          
+          fetchDataPushDireccion(dir2.POSICION,dir2.IDNOMEN,dir2.VALORDIRECC);     
+        }, 2000);        
+
+        setTimeout(() => {          
+          fetchDataPushDireccion(dir3.POSICION,dir3.IDNOMEN,dir3.VALORDIRECC);     
+        }, 2000);   
+        
+
+        setTimeout(() => {          
+          fetchDataPushDireccion(dir4.POSICION,dir4.IDNOMEN,dir4.VALORDIRECC);     
+        }, 2000);        
+        
+
+        setTimeout(() => {          
+          fetchDataPushDireccion(dir5.POSICION,dir5.IDNOMEN,dir5.VALORDIRECC);     
+        }, 2000);        
+        
+
+        setTimeout(() => {          
+          fetchDataPushDireccion(dir6.POSICION,dir6.IDNOMEN,dir6.VALORDIRECC);     
+        }, 2000);        
+        
+
+        setTimeout(() => {          
+          fetchDataPushDireccion(dir7.POSICION,dir7.IDNOMEN,dir7.VALORDIRECC);     
+        }, 2000);        
+        
+
+        setTimeout(() => {          
+          fetchDataPushDireccion(dir8.POSICION,dir8.IDNOMEN,dir8.VALORDIRECC);     
+        }, 2000);        
+        
+
+        setTimeout(() => {          
+          fetchDataPushDireccion(dir9.POSICION,dir9.IDNOMEN,dir9.VALORDIRECC);     
+        }, 2000);        
+        
+
+        setTimeout(() => {          
+          fetchDataPushDireccion(dir10.POSICION,dir10.IDNOMEN,dir10.VALORDIRECC);     
+        }, 2000);        
+       
+
+        setTimeout(() => {          
+          fetchDataPushDireccion(dir11.POSICION,dir11.IDNOMEN,dir11.VALORDIRECC);     
+        }, 2000);        
+        
+
+        setTimeout(() => {          
+          fetchDataPushDireccion(dir12.POSICION,dir12.IDNOMEN,dir12.VALORDIRECC);     
+        }, 2000);        
+       
+
+        setTimeout(() => {          
+          fetchDataPushDireccion(dir13.POSICION,dir13.IDNOMEN,dir13.VALORDIRECC);     
+        }, 2000);        
+        
+
+        setTimeout(() => {          
+          fetchDataPushDireccion(dir14.POSICION,dir14.IDNOMEN,dir14.VALORDIRECC);     
+        }, 2000);        
+        
+
+        setTimeout(() => {          
+          fetchDataPushDireccion(dir15.POSICION,dir15.IDNOMEN,dir15.VALORDIRECC);     
+        }, 2000);        
+        
+
+        setTimeout(() => {          
+          fetchDataPushDireccion(dir16.POSICION,dir16.IDNOMEN,dir16.VALORDIRECC);     
+        }, 2000);        
+        
+
+        setTimeout(() => {          
+          fetchDataPushDireccion(dir17.POSICION,dir17.IDNOMEN,dir17.VALORDIRECC);     
+        }, 2000);        
+        
+
+        setTimeout(() => {          
+          fetchDataPushDireccion(dir18.POSICION,dir18.IDNOMEN,dir18.VALORDIRECC);     
+        }, 2000);        
+        
+
+        setTimeout(() => {          
+          fetchDataPushDireccion(dir19.POSICION,dir19.IDNOMEN,dir19.VALORDIRECC);     
+        }, 2000);        
+        
+
+        setTimeout(() => {          
+          fetchDataPushDireccion(dir20.POSICION,dir20.IDNOMEN,dir20.VALORDIRECC);     
+        }, 2000);        
+        
+
+        setTimeout(() => {          
+          fetchDataPushDireccion(dir21.POSICION,dir21.IDNOMEN,dir21.VALORDIRECC);     
+        }, 2000);        
+        
+     
+
+
+        } catch (error) {
+        console.error('Error al verificar o enviar el registro:', error);
+        }
     }
 
   return (
@@ -214,29 +390,36 @@ export function Registrar() {
             {/*----------------------------Contactos----------------------------*/}  
             {contacto.map((contactoItem, index) => (              
               <div key={index} className="contacto-item">
-                <select value={contactoItem.IDTIPOCONTACTO} 
+                <select
+                  value={JSON.stringify({ IDTIPOCONTACTO: contactoItem.IDTIPOCONTACTO, DESCTIPOCONTACTO: contactoItem.DESCTIPOCONTACTO })}
                   onChange={(event) => {
-                    const updatedContacto = [...contacto]; // Haciendo una copia del estado actual
+                    const { IDTIPOCONTACTO, DESCTIPOCONTACTO } = JSON.parse(event.target.value);
+                    const updatedContacto = [...contacto];
                     updatedContacto[index] = {
                       ...updatedContacto[index],
-                      IDTIPOCONTACTO: event.target.value,
+                      IDTIPOCONTACTO,
+                      DESCTIPOCONTACTO
                     };
-                    setContacto(updatedContacto); // Actualizando el estado con el nuevo valor
+                    setContacto(updatedContacto);
                   }}
                 >
                   <option value="">TipoContacto</option>
                   {tipocontacto.map((docuOption) => (
-                    <option key={docuOption.IDTIPOCONTACTO} value={docuOption.IDTIPOCONTACTO}>
+                    <option
+                      key={docuOption.IDTIPOCONTACTO}
+                      value={JSON.stringify({ IDTIPOCONTACTO: docuOption.IDTIPOCONTACTO, DESCTIPOCONTACTO: docuOption.DESCTIPOCONTACTO })}
+                    >
                       {docuOption.DESCTIPOCONTACTO}
                     </option>
                   ))}
                 </select>
+
                 <input type="text" value={contactoItem.valor} 
                   onChange={(e) => {
                     const updatedContacto = [...contacto]; // Haciendo una copia del estado actual
                     updatedContacto[index] = {
                       ...updatedContacto[index],
-                      valor: e.target.value,
+                      DESCCONTACTO: e.target.value,
                     };
                     setContacto(updatedContacto); // Actualizando el estado con el nuevo valor
                   }}
@@ -405,6 +588,7 @@ export function Registrar() {
           </div>
         </div>
         <button className="registrar-btn">Registrar</button>
+        {confirmationMessage && <div className="confirmation-message">{confirmationMessage}</div>}
       </form>  
     </div>
   );
